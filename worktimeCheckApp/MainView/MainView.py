56f1ec9c38395ea5cd2ad01eb4f563a08f499dad
Henry O <somondon@naver.com>
2018-06-23 15:25:54 +0900

    add menu bar

--- worktimeCheckApp/MainView/MainView.py
+++ worktimeCheckApp/MainView/MainView.py
@@ -4,16 +4,37 @@ from PyQt5 import *
 from PyQt5.QtWidgets import *
 
 
-class gridlayout_example(QWidget):
-    def __init__(self, parent=None):
-        super(gridlayout_example, self).__init__(parent)
+class MainView(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.mainUI = createMainUi()
+        self.setCentralWidget(self.mainUI)
+        self.setWindowTitle("Henry's WorkTime")
 
+        menu_bar = self.menuBar()
+        menu_bar.setNativeMenuBar(False)
+        file_menu = menu_bar.addMenu('File')
+        edit_menu = menu_bar.addMenu('Edit')
+        new_action = QAction('New', self)
+        file_menu.addAction(new_action)
+        new_action.setStatusTip("New File")
+
+        self.resize(400, 300)
+        self.statusBar().showMessage("Text in status bar")
+
+
+
+class createMainUi(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        menubar = QMenuBar()
+        menubar.setNativeMenuBar(False)
         cal = QCalendarWidget()
         cal.setVerticalHeaderFormat(0)  # vertical header 숨기기
 
         sublayout1 = QVBoxLayout()
         sublayout1.addWidget(cal)
-
 
         label2 = QLabel('remain Time!!')
         line_edit2 = QLineEdit()
@@ -35,7 +56,7 @@ class gridlayout_example(QWidget):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    myWindow = gridlayout_example()
+    myWindow = MainView()
 
     myWindow.show()
 
