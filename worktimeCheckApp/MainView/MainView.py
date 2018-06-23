b74d61587d8f3da8861b1e398e7e7c6eefd9c83c
Henry O <somondon@naver.com>
2018-06-23 17:52:27 +0900

    add timer

--- worktimeCheckApp/MainView/MainView.py
+++ worktimeCheckApp/MainView/MainView.py
@@ -1,7 +1,59 @@
+import datetime
 import sys
 
 from PyQt5 import *
+from PyQt5.QtCore import QDate, QEvent, Qt, QTimer, QTime
+from PyQt5.QtGui import QIcon, QCursor
 from PyQt5.QtWidgets import *
+
+
+class timeSelectDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setupUI()
+
+        self.id = None
+        self.password = None
+
+    def setupUI(self):
+        self.setGeometry(400, 400, 300, 100)
+        self.setWindowTitle("Sign In")
+        self.setWindowIcon(QIcon('icon.png'))
+        self.setWindowFlags(self.windowFlags() | Qt.FramelessWindowHint)
+        self.StartTimeed = QTimeEdit(self)
+        self.StartTimeed.setDisplayFormat("hh:mm:ss")  # 24 시간으로 표시 self.timeed.setTime(QTime(15,30))
+        self.EndTimeed = QTimeEdit(self)
+        self.EndTimeed.setDisplayFormat("hh:mm:ss")  # 24 시간으로 표시 self.timeed.setTime(QTime(15,30))
+        # 현재 시간으로 Default 설정 추가 필요
+
+        label1 = QLabel("Start Work: ")
+        label2 = QLabel("End Work: ")
+
+        self.lineEdit1 = QLineEdit()
+        self.lineEdit2 = QLineEdit()
+        self.pushButton1 = QPushButton("Ok")
+        self.pushButton1.clicked.connect(self.pushButtonClicked)
+
+        layout = QGridLayout()
+        layout.addWidget(label1, 0, 0)
+        layout.addWidget(self.StartTimeed, 0, 1)
+        layout.addWidget(self.pushButton1, 0, 2)
+        layout.addWidget(label2, 1, 0)
+        layout.addWidget(self.EndTimeed, 1, 1)
+
+        self.setLayout(layout)
+
+    def pushButtonClicked(self):
+        self.startTime= self.StartTimeed.time()
+        self.endTime = self.EndTimeed.time()
+        self.hide()
+
+    def closeEvent(self, event):
+        print
+        "User has clicked the red x on the main window"
+        event.accept()
+
+
 
 
 class MainView(QMainWindow):
@@ -32,26 +84,49 @@ class createMainUi(QWidget):
         menubar.setNativeMenuBar(False)
         cal = QCalendarWidget()
         cal.setVerticalHeaderFormat(0)  # vertical header 숨기기
+        cal.installEventFilter(self)
+
+        cal.clicked[QDate].connect(self.showDayInfo)
 
         sublayout1 = QVBoxLayout()
         sublayout1.addWidget(cal)
 
         label2 = QLabel('remain Time!!')
+        label3 = QLabel('Time')
         line_edit2 = QLineEdit()
         sublayout2 = QVBoxLayout()
         sublayout2.addWidget(label2)
         sublayout2.addWidget(line_edit2)
-
+        sublayout2.addWidget(label3)
         button1 = QPushButton("button1")
         button2 = QPushButton("button2")
         button3 = QPushButton("button3")
-
         grid_layout = QGridLayout(self)
         grid_layout.addLayout(sublayout1, 0, 0, 1, 3)
         grid_layout.addLayout(sublayout2, 1, 0, 1, 3)
         grid_layout.addWidget(button1, 2, 0, 1, 1)
         grid_layout.addWidget(button2, 2, 1, 1, 1)
         grid_layout.addWidget(button3, 2, 2, 1, 1)
+
+    def showDayInfo(self):
+        dlg = timeSelectDialog()
+        dlg.exec_()
+        self.timer_start(dlg.startTime.toString('hh:mm:ss'))
+
+
+    def timer_start(self,startTime):
+        print(startTime)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timerEvent(startTime))
+        self.timer.start(1000)
+
+        # self.update_gui()
+
+    def timerEvent(self,startTime):
+        now = QTime.currentTime().toString('hh:mm:ss')
+        # hour = startTime - now
+        # print(hour)
+        print(now)
 
 
 if __name__ == "__main__":
