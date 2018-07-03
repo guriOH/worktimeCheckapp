49482f82be91a5a63ec1efd68775876adf09f0c7
Henry O <ohoon@bistel.com>
2018-07-03 18:57:31 +0900

    Add Properties .py

---
+++ worktimeCheckApp/MainView/Config.py
@@ -0,0 +1,37 @@
+import configparser
+
+class PropertiesReader(object):
+
+    def __init__(self):
+        self.name = "../path.properties"
+        self.main_section = 'PATH'
+
+        # Add dummy section on top
+        self.lines = [ '[%s]\n' % self.main_section ]
+
+        with open(self.name) as f:
+            self.lines.extend(f.readlines())
+
+        # This makes sure that iterator in readfp stops
+        self.lines.append('')
+
+    def readline(self):
+        return self.lines.pop(0)
+
+    def read_properties(self):
+
+        config = configparser.RawConfigParser()
+        config.read(self.name)
+
+        details_dict = dict(config.items(self.main_section))
+        return details_dict
+
+
+props = PropertiesReader()
+
+dic_props = props.read_properties()
+
+print(dic_props)
+
+print(dic_props.get('ui.spark.savepath'))
+print(dic_props)
