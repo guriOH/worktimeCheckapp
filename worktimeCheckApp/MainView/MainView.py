c91fe80356d311507906c2c1f5fb0bb79952ec33
Henry O <somondon@naver.com>
2018-07-29 12:58:44 +0900

    Divide View

--- worktimeCheckApp/MainView/MainView.py
+++ worktimeCheckApp/MainView/MainView.py
@@ -5,72 +5,14 @@ from PyQt5.QtGui import QIcon, QCursor, QTextCharFormat, QMouseEvent
 from PyQt5.QtWidgets import *
 from worktimeCheckApp.Config.Config import *
 from worktimeCheckApp.DataBase.dbUtils import *
+from worktimeCheckApp.MainView.Dialog.timeSelectDlg import *
 import datetime
 
 
 
-startWorkTime = None
+
 remainTime_one_week = 144000.0
 t = ['월', '화', '수', '목', '금', '토', '일']
-con = None
-scheduletableName = "schedule"
-
-class timeSelectDialog(QDialog):
-    def __init__(self):
-        super().__init__()
-        self.setupUI()
-
-
-    def setupUI(self):
-        self.setGeometry(400, 400, 300, 100)
-        self.setWindowTitle("Sign In")
-        self.setWindowIcon(QIcon('icon.png'))
-        self.setWindowFlags(self.windowFlags() | Qt.FramelessWindowHint)
-        self.StartTimeed = QTimeEdit(self)
-        self.StartTimeed.setDisplayFormat("hh:mm:ss")  # 24 시간으로 표시 self.timeed.setTime(QTime(15,30))
-        self.EndTimeed = QTimeEdit(self)
-        self.EndTimeed.setDisplayFormat("hh:mm:ss")  # 24 시간으로 표시 self.timeed.setTime(QTime(15,30))
-        # 현재 시간으로 Default 설정 추가 필요
-
-        if startWorkTime is None:
-            self.StartTimeed.setTime(QTime.currentTime())
-        else:
-            self.StartTimeed.setTime(startWorkTime)
-        label1 = QLabel("Start Work: ")
-        label2 = QLabel("End Work: ")
-
-        self.lineEdit1 = QLineEdit()
-        self.lineEdit2 = QLineEdit()
-        self.pushButton1 = QPushButton("Ok")
-        self.pushButton1.clicked.connect(self.pushButtonClicked)
-
-        layout = QGridLayout()
-        layout.addWidget(label1, 0, 0)
-        layout.addWidget(self.StartTimeed, 0, 1)
-        layout.addWidget(self.pushButton1, 0, 2)
-        layout.addWidget(label2, 1, 0)
-        layout.addWidget(self.EndTimeed, 1, 1)
-
-        self.setLayout(layout)
-
-    def pushButtonClicked(self):
-        self.startWorkTime = self.StartTimeed.time()
-        self.endWorkTime = self.EndTimeed.time()
-        global startWorkTime
-        startWorkTime = self.StartTimeed.time()
-        global endWorkTime
-        endWorkTime = self.EndTimeed.time()
-        print(startWorkTime)
-        print(endWorkTime)
-        self.close()
-        self.reject()
-
-    def closeEvent(self, event):
-        print
-        "User has clicked the red x on the main window"
-        event.accept()
-
-
 
 class MainView(QMainWindow):
     def __init__(self):
